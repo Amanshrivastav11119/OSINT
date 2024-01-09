7255f8d5581a993349dfa58e0152e827b389fbb7
John <johnmalfoy112@gmail.com>
2024-01-09 14:05:32 +0530

    osint

--- src/app/service/facebook.service.ts
+++ src/app/service/facebook.service.ts
@@ -3,17 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class FacebookService {
 
     private baseUrl = 'http://localhost:3000/api/facebook/news';
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
-  searchFacebookData(keywords: string): Observable<any> {
-    const url = `${this.baseUrl}?keywords=${keywords}`;
-    return this.http.get(url);
-  }
+    searchFacebookData(keywords: string): Observable<any> {
+        const url = `${this.baseUrl}?keywords=${keywords}`;
+        return this.http.get(url);
+    }
 
 }
