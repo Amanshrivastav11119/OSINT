a6204d4cc507148ce2704a80df18d7158cd935dc
John <johnmalfoy112@gmail.com>
2024-01-11 11:34:09 +0530

    osint

--- src/app/blockchain/blockchain.component.ts
+++ src/app/blockchain/blockchain.component.ts
@@ -1,6 +1,38 @@
 import { Component } from '@angular/core';
 import { BlockchainService } from '../service/blockchain.service';
 
+interface BalanceResult {
+  final_balance: number;
+  n_tx: number;
+  total_received: number;
+}
+
+interface AddressInfo {
+  address: string;
+  final_balance: number;
+  n_tx: number;
+  total_received: number;
+}
+
+interface TransactionInfo {
+  txid: string;
+  // Add more properties as needed
+}
+
+interface GeneralInfo {
+  nconnected: number;
+  conversion: number;
+  // Add more properties as needed
+}
+
+interface MultipleAddressResult {
+  addresses: AddressInfo[];
+  wallet: BalanceResult;
+  txs: TransactionInfo[];
+  info: GeneralInfo;
+  recommend_include_fee: boolean;
+}
+
 @Component({
   selector: 'app-blockchain',
   templateUrl: './blockchain.component.html',
@@ -10,24 +42,21 @@ export class BlockchainComponent {
 
   userInput: string;
   selectedFunction: string = "Select Option";
-  searchResult: any;
+  searchResult: undefined | MultipleAddressResult | any;
 
-    // Search placeholder functions
-    isFocused: boolean = false;
-    onFocus() {
-      this.isFocused = true;
-    }
-    onBlur() {
-      this.isFocused = false;
-    }
+  // Search placeholder functions
+  isFocused: boolean = false;
+  onFocus() {
+    this.isFocused = true;
+  }
+  onBlur() {
+    this.isFocused = false;
+  }
 
-  constructor(private blockchainService: BlockchainService) {}
+  constructor(private blockchainService: BlockchainService) { }
 
   search(): void {
     switch (this.selectedFunction) {
-      case 'Balance':
-        this.getBalance(this.userInput);
-        break;
       case 'Multiple Address':
         this.getMultiaddr(this.userInput);
         break;
@@ -45,28 +74,29 @@ export class BlockchainComponent {
     }
   }
 
-  selectFunction(func: string): void {  // Change the method name to 'selectFunction'
-    this.selectedFunction = func;
-  }
-
-  getBalance(active: string): void {
-    this.blockchainService.getBalance(active).subscribe(result => this.searchResult = result);
+  selectFunction(selectedFunction: string) {
+    this.selectedFunction = selectedFunction;
+    this.searchResult = undefined; 
   }
 
   getMultiaddr(active: string): void {
-    this.blockchainService.getMultiaddr(active).subscribe(result => this.searchResult = result);
+    this.blockchainService.getMultiaddr(active).subscribe((result: MultipleAddressResult) => this.searchResult = result);
+    console.log(this.searchResult);
   }
 
   getRawaddr(address: string): void {
     this.blockchainService.getRawaddr(address).subscribe(result => this.searchResult = result);
+    console.log(this.searchResult);
   }
 
   getRawtx(txid: string): void {
     this.blockchainService.getRawtx(txid).subscribe(result => this.searchResult = result);
+    console.log(this.searchResult);
   }
 
   getRawblock(blockhash: string): void {
     this.blockchainService.getRawblock(blockhash).subscribe(result => this.searchResult = result);
+    console.log(this.searchResult);
   }
 
 }
